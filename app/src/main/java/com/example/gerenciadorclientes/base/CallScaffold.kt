4f066e0ed5e109c53df95e95104d15c4f6f8e856
ricardodelvecchio <ric.vecchio@gmail.com>
2025-05-19 14:19:36 -0300

    Incluindo imagem de cabeçalho.

--- app/src/main/java/com/example/gerenciadorclientes/base/CallScaffold.kt
+++ app/src/main/java/com/example/gerenciadorclientes/base/CallScaffold.kt
@@ -1,4 +1,8 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -10,12 +14,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.heightIn
+import com.example.gerenciadorclientes.R
 import com.example.gerenciadorclientes.base.Routes
 import com.example.gerenciadorclientes.model.SharedPreferences
 import com.example.gerenciadorclientes.screens.CreateClientScreen
 import com.example.gerenciadorclientes.screens.CreateClientViewModel
-import com.example.gerenciadorclientes.screens.EditClientScreen
 import com.example.gerenciadorclientes.screens.EditClientViewModel
 import com.example.gerenciadorclientes.screens.ListClientScreen
 import com.example.gerenciadorclientes.screens.ListClientViewModel
@@ -30,42 +40,39 @@ class CallScaffold(val navController: NavController, val localData: SharedPrefer
     fun CreateScreen(screen: String, additionalContent: @Composable (() -> Unit)? = null) {
         Scaffold(
             topBar = {
-                when (screen) {
-                    Routes.ClientList.route -> {
-                        CenterAlignedTopAppBar(
-                            title = { Text(text = "Lista de Clientes") },
-                        )
-                    }
-                    Routes.ClientCreate.route -> {
-                        CenterAlignedTopAppBar(
-                            title = { Text(text = "Cadastrar Cliente") },
-                            navigationIcon = {
+                Column {
+                    Image(
+                        painter = painterResource(id = R.drawable.clientes),
+                        contentDescription = "Logo Clientes",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 120.dp)
+                            .aspectRatio(16 / 9f),
+                        contentScale = ContentScale.FillWidth
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+                    CenterAlignedTopAppBar(
+                        title = {
+                            when (screen) {
+                                Routes.ClientList.route -> Text(text = "Lista de Clientes")
+                                Routes.ClientCreate.route -> Text(text = "Cadastrar Cliente")
+                                Routes.ClientEdit.route -> Text(text = "Editar Cliente")
+                                Routes.ClientDetails.route -> Text(text = "Detalhes do Cliente")
+                            }
+                        },
+                        navigationIcon = {
+                            if (screen != Routes.ClientList.route) {
                                 IconButton(onClick = { navController.popBackStack() }) {
-                                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Voltar")
+                                    Icon(
+                                        Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "Voltar"
+                                    )
                                 }
                             }
-                        )
-                    }
-                    Routes.ClientEdit.route -> {
-                        CenterAlignedTopAppBar(
-                            title = { Text(text = "Editar Cliente") },
-                            navigationIcon = {
-                                IconButton(onClick = { navController.popBackStack() }) {
-                                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Voltar")
-                                }
-                            }
-                        )
-                    }
-                    Routes.ClientDetails.route -> {
-                        CenterAlignedTopAppBar(
-                            title = { Text(text = "Detalhes do Cliente") },
-                            navigationIcon = {
-                                IconButton(onClick = { navController.popBackStack() }) {
-                                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Voltar")
-                                }
-                            }
-                        )
-                    }
+                        },
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
                 }
             }
         ) { padding ->
@@ -74,11 +81,10 @@ class CallScaffold(val navController: NavController, val localData: SharedPrefer
                     Routes.ClientList.route -> ListClientScreen(padding, navController, listClientViewModel)
                     Routes.ClientCreate.route -> CreateClientScreen(padding, navController, createClientViewModel)
                     Routes.ClientEdit.route -> {
+                        additionalContent?.invoke()
                     }
                 }
-                additionalContent?.invoke()
             }
         }
     }
-
 }
