a177e3d79d09deebbafaa2eee5f190f81f9886a8
ricardodelvecchio <ric.vecchio@gmail.com>
2025-05-19 14:40:18 -0300

    Ajustando imagem do cabeçalho e formatação.

--- app/src/main/java/com/example/gerenciadorclientes/base/CallScaffold.kt
+++ app/src/main/java/com/example/gerenciadorclientes/base/CallScaffold.kt
@@ -1,26 +1,29 @@
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.heightIn
 import com.example.gerenciadorclientes.R
 import com.example.gerenciadorclientes.base.Routes
 import com.example.gerenciadorclientes.model.SharedPreferences
@@ -40,46 +43,72 @@ class CallScaffold(val navController: NavController, val localData: SharedPrefer
     fun CreateScreen(screen: String, additionalContent: @Composable (() -> Unit)? = null) {
         Scaffold(
             topBar = {
-                Column {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.primary)
+                ) {
                     Image(
                         painter = painterResource(id = R.drawable.clientes),
                         contentDescription = "Logo Clientes",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .heightIn(min = 120.dp)
+                            .heightIn(min = 280.dp)
                             .aspectRatio(16 / 9f),
                         contentScale = ContentScale.FillWidth
                     )
 
                     Spacer(modifier = Modifier.height(8.dp))
-                    CenterAlignedTopAppBar(
-                        title = {
-                            when (screen) {
-                                Routes.ClientList.route -> Text(text = "Lista de Clientes")
-                                Routes.ClientCreate.route -> Text(text = "Cadastrar Cliente")
-                                Routes.ClientEdit.route -> Text(text = "Editar Cliente")
-                                Routes.ClientDetails.route -> Text(text = "Detalhes do Cliente")
-                            }
+
+                    Text(
+                        text = when (screen) {
+                            Routes.ClientList.route -> "Lista de Clientes"
+                            Routes.ClientCreate.route -> "Cadastrar Cliente"
+                            Routes.ClientEdit.route -> "Editar Cliente"
+                            Routes.ClientDetails.route -> "Detalhes do Cliente"
+                            else -> ""
                         },
-                        navigationIcon = {
-                            if (screen != Routes.ClientList.route) {
+                        style = MaterialTheme.typography.headlineMedium.copy(color = MaterialTheme.colorScheme.onPrimary),
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(bottom = 8.dp)
+                    )
+
+                    if (screen != Routes.ClientList.route) {
+                        TopAppBar(
+                            title = {},
+                            navigationIcon = {
                                 IconButton(onClick = { navController.popBackStack() }) {
                                     Icon(
                                         Icons.AutoMirrored.Filled.ArrowBack,
-                                        contentDescription = "Voltar"
+                                        contentDescription = "Voltar",
+                                        tint = MaterialTheme.colorScheme.onPrimary
                                     )
                                 }
-                            }
-                        },
-                        modifier = Modifier.padding(vertical = 4.dp)
-                    )
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                            )
+                        )
+                    }
                 }
             }
         ) { padding ->
             Box(modifier = Modifier.padding(padding)) {
                 when (screen) {
-                    Routes.ClientList.route -> ListClientScreen(padding, navController, listClientViewModel)
-                    Routes.ClientCreate.route -> CreateClientScreen(padding, navController, createClientViewModel)
+                    Routes.ClientList.route -> ListClientScreen(
+                        padding,
+                        navController,
+                        listClientViewModel
+                    )
+
+                    Routes.ClientCreate.route -> CreateClientScreen(
+                        padding,
+                        navController,
+                        createClientViewModel
+                    )
+
                     Routes.ClientEdit.route -> {
                         additionalContent?.invoke()
                     }
